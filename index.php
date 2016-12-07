d118dd1fc772c32ca4a1c237e665ce84049e0cce
Illya Moskvin <imoskvin@imamuseum.org>
2016-12-07 05:30:32 -0500

    Added code to parse Emu dump and keep only the AAC objects

--- index.php
+++ index.php
@@ -70,3 +70,78 @@ class ObjectIterator extends XMLElementIterator {
         return simplexml_load_string($this->reader->readOuterXml(), "ObjectElement", LIBXML_NOCDATA);
     }
 }
+
+// http://stackoverflow.com/questions/4778865/php-simplexml-addchild-with-another-simplexmlelement
+function simplexml_append(SimpleXMLElement $to, SimpleXMLElement $from) {
+    $toDom = dom_import_simplexml($to);
+    $fromDom = dom_import_simplexml($from);
+    $toDom->appendChild($toDom->ownerDocument->importNode($fromDom, true));
+}
+
+// Parse the original EMU XML, keep only objects w/ irns that match AAC's
+// We are doing this mainly to speed up future processing
+if( !file_exists( FILE_EMU_NEW ) ) {
+
+    // We are going to output the results as we go along
+    header("Content-Type: text/plain");
+
+    // Start output buffering if it's disabled
+    // http://php.net/manual/en/function.ob-get-level.php
+    if( !ob_get_level() ) {
+        ob_start();
+    }
+
+    $reader = new XMLReader();
+    $reader->open( FILE_EMU_OLD );
+
+    // Inefficient, but we need to do an Xpath query a la in_array for irns
+    $ids = file_get_contents( FILE_IDS );
+    $ids = json_decode( $ids );
+
+    // Uncomment for testing
+    // $ids = array_slice( $ids, 0, 5 );
+
+    /* @var $users XMLReaderNode[] - iterate over all <object> elements */
+    $objects = new ObjectIterator($reader);
+
+    // This variable will be added to the response
+    $filtered = array();
+
+    foreach( $objects as $object ) {
+
+        if( in_array( $object->meta('irn'), $ids ) ) {
+
+            $filtered[] = $object;
+
+            echo $object->meta('TitAccessionNo') . PHP_EOL;
+
+            ob_flush();
+            flush();
+
+            // Uncomment for testing
+            // if( $objects->key() > 5 ) break;
+
+        }
+
+    }
+
+    // Build output to match the Dagwood export file
+    $result = new SimpleXMLElement('<response></response>');
+    $status = $result->addChild( 'status', 'ok' );
+    $total = $result->addChild( 'totalItems', count($filtered) );
+
+    foreach( $filtered as $object ) {
+        simplexml_append( $result, $object );
+    }
+
+    // Possible but unnecessary improvement:
+    // http://stackoverflow.com/questions/1191167/format-output-of-simplexml-asxml
+    $out = $result->asXML();
+
+    file_put_contents( FILE_EMU_NEW, $out );
+
+    echo "Done.";
+
+    ob_end_flush();
+
+}
